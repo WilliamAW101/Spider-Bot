5191007b799119a4000bcd6c7138b9ff296b1ebc
squallcoder <tammapavan38@gmail.com>
2025-11-16 16:07:01 -0500

    Fixed issue

--- connect.py
+++ connect.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-arduino = serial.Serial('/dev/ttyAMAO', 9600, timeout=1)
+arduino = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
 time.sleep(2)
 cmd = "Connected"
 arduino.write(cmd.encode())
